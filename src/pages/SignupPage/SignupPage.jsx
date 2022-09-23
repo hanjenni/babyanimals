a5004bbc11f7578c2c318b91d03e7adc2e373381
username <youremail@domain.com>
2022-09-23 16:50:59 -0700

    signup page

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,20 +1,66 @@
 import React, { useState } from "react";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import { Button, Form, Grid, Header, Image, Segment, Message } from 'semantic-ui-react';
 
 import userService from "../../utils/userService";
 import { useNavigate } from "react-router-dom";
 
 export default function SignUpPage(props) {
+  const [error, setError] = useState('');
+  const [state, setState] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConf:''
+
+  })
   return (
-    <>
-      <h1>Signup PAGE</h1>
-      <ul>
-        <li>Read the Login Model, You can change it to fit your needs</li>
-        <li>
-          Make sure you read the Login Controller, to know how it is setup to
-          find the user!
-        </li>
-      </ul>
-    </>
+    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+    <Grid.Column style={{ maxWidth: 450 }}>
+      <Header as='h2' color='yellow' textAlign='center'>
+        <Image src='/logo.png' /> Log-in to your account
+      </Header>
+      <Form size='large'>
+        <Segment stacked>
+          <Form.Input 
+            name='username'
+            value=''
+            iconPosition='left' 
+            placeholder='username' 
+            fluid icon='user'
+          />
+          <Form.Input
+            name='email'
+            type='email'
+            value=''
+            iconPosition='left'
+            placeholder='email'
+            fluid icon='lock'
+          />
+          <Form.Input
+            name='password'
+            type='password'
+            iconPosition='left'
+            placeholder='Password'
+            
+          />
+          <Form.Input
+            name='passwordConf'
+            type='password'
+            iconPosition='left'
+            placeholder='Confirm Password'
+            
+          />
+
+          <Button color='yellow' fluid size='large'>
+            Login
+          </Button>
+        </Segment>
+      </Form>
+      <Message>
+        New to us? <a href='#'>Sign Up</a>
+      </Message>
+    </Grid.Column>
+  </Grid>
   );
 }
